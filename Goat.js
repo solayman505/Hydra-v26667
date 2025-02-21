22a0236edff65341f8aebf225c046ff8f079747b
pro004 <108160357+pro004@users.noreply.github.com>
2025-02-21 15:00:19 +0600

    Update Goat.js

--- Goat.js
+++ Goat.js
@@ -1,190 +1,59 @@
-process.on('unhandledRejection', error => console.log(error));
-process.on('uncaughtException', error => console.log(error));
+process.on('unhandledRejection', error => console.log(error)); process.on('uncaughtException', error => console.log(error));
 
-const axios = require("axios");
-const fs = require("fs-extra");
-const google = require("googleapis").google;
-const nodemailer = require("nodemailer");
-const { execSync } = require('child_process');
-const log = require('./logger/log.js');
-const path = require("path");
+const axios = require("axios"); const fs = require("fs-extra"); const google = require("googleapis").google; const nodemailer = require("nodemailer"); const { execSync } = require('child_process'); const log = require('./logger/log.js'); const path = require("path");
 
-process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0; // Disable warning: "Warning: a promise was created in a handler but was not returned from it"
+process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0;
 
-function validJSON(pathDir) {
-	try {
-		if (!fs.existsSync(pathDir))
-			throw new Error(`File "${pathDir}" not found`);
-		execSync(`npx jsonlint "${pathDir}"`, { stdio: 'pipe' });
-		return true;
-	}
-	catch (err) {
-		let msgError = err.message;
-		msgError = msgError.split("\n").slice(1).join("\n");
-		const indexPos = msgError.indexOf("    at");
-		msgError = msgError.slice(0, indexPos != -1 ? indexPos - 1 : msgError.length);
-		throw new Error(msgError);
-	}
-}
+function validJSON(pathDir) { try { if (!fs.existsSync(pathDir)) throw new Error(File "${pathDir}" not found); execSync(npx jsonlint "${pathDir}", { stdio: 'pipe' }); return true; } catch (err) { let msgError = err.message.split("\n").slice(1).join("\n"); const indexPos = msgError.indexOf("    at"); msgError = msgError.slice(0, indexPos != -1 ? indexPos - 1 : msgError.length); throw new Error(msgError); } }
 
-const { NODE_ENV } = process.env;
-const dirConfig = path.normalize(`${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`);
-const dirConfigCommands = path.normalize(`${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}`);
-const dirAccount = path.normalize(`${__dirname}/account${['production', 'development'].includes(NODE_ENV) ? '.dev.txt' : '.txt'}`);
+const { NODE_ENV } = process.env; const dirConfig = path.normalize(${__dirname}/config${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}); const dirConfigCommands = path.normalize(${__dirname}/configCommands${['production', 'development'].includes(NODE_ENV) ? '.dev.json' : '.json'}); const dirAccount = path.normalize(${__dirname}/account${['production', 'development'].includes(NODE_ENV) ? '.dev.txt' : '.txt'});
 
-for (const pathDir of [dirConfig, dirConfigCommands]) {
-	try {
-		validJSON(pathDir);
-	}
-	catch (err) {
-		log.error("CONFIG", `Invalid JSON file "${pathDir.replace(__dirname, "")}":\n${err.message.split("\n").map(line => `  ${line}`).join("\n")}\nPlease fix it and restart bot`);
-		process.exit(0);
-	}
-}
-const config = require(dirConfig);
-if (config.whiteListMode?.whiteListIds && Array.isArray(config.whiteListMode.whiteListIds))
-	config.whiteListMode.whiteListIds = config.whiteListMode.whiteListIds.map(id => id.toString());
-const configCommands = require(dirConfigCommands);
+for (const pathDir of [dirConfig, dirConfigCommands]) { try { validJSON(pathDir); } catch (err) { log.error("CONFIG", Invalid JSON file "${pathDir.replace(__dirname, "")}":\n${err.message.split("\n").map(line =>   ${line}).join("\n")}\nPlease fix it and restart bot); process.exit(0); } }
 
-global.GoatBot = {
- 	commands: new Map(),
-    	events: new Map(),
-   	cooldowns: new Map(),
-   	eventRegistered: new Array(),
-    	handleSchedule: new Array(),
-    	handleReaction: new Array(),
-    	handleReply: new Array(),
-    	mainPath: process.cwd(),
-    	configPath: new String()
-	startTime: Date.now() - process.uptime() * 1000, // time start bot (ms)
-	commands: new Map(), // store all commands
-	eventCommands: new Map(), // store all event commands
-	commandFilesPath: [], // [{ filePath: "", commandName: [] }
-	eventCommandsFilesPath: [], // [{ filePath: "", commandName: [] }
-	aliases: new Map(), // store all aliases
-	onFirstChat: [], // store all onFirstChat [{ commandName: "", threadIDsChattedFirstTime: [] }}]
-	onChat: [], // store all onChat
-	onEvent: [], // store all onEvent
-	onReply: new Map(), // store all onReply
-	onReaction: new Map(), // store all onReaction
-	onAnyEvent: [], // store all onAnyEvent
-	config, // store config
-	configCommands, // store config commands
-	envCommands: {}, // store env commands
-	envEvents: {}, // store env events
-	envGlobal: {}, // store env global
-	reLoginBot: function () { }, // function relogin bot, will be set in bot/login/login.js
-	Listening: null, // store current listening handle
-	oldListening: [], // store old listening handle
-	callbackListenTime: {}, // store callback listen 
-	storage5Message: [], // store 5 message to check listening loop
-	fcaApi: null, // store fca api
-	botID: null // store bot id
+const config = require(dirConfig); if (config.whiteListMode?.whiteListIds && Array.isArray(config.whiteListMode.whiteListIds)) config.whiteListMode.whiteListIds = config.whiteListMode.whiteListIds.map(id => id.toString()); const configCommands = require(dirConfigCommands);
+
+global.GoatBot = { commands: new Map(), events: new Map(), cooldowns: new Map(), eventRegistered: [], handleSchedule: [], handleReaction: [], handleReply: [], mainPath: process.cwd(), configPath: new String(), startTime: Date.now() - process.uptime() * 1000, // Fixed missing comma commandFilesPath: [], eventCommandsFilesPath: [], aliases: new Map(), onFirstChat: [], onChat: [], onEvent: [], onReply: new Map(), onReaction: new Map(), onAnyEvent: [], config, configCommands, envCommands: {}, envEvents: {}, envGlobal: {}, reLoginBot: function () {}, Listening: null, oldListening: [], callbackListenTime: {}, storage5Message: [], fcaApi: null, botID: null };
+
+global.db = { threadInfo: new Map(), threadData: new Map(), userName: new Map(), userBanned: new Map(), threadBanned: new Map(), commandBanned: new Map(), threadAllowNSFW: [], allUserID: [], allCurrenciesID: [], allThreadID: [], allThreadData: [], allUserData: [], allDashBoardData: [], allGlobalData: [], threadModel: null, userModel: null, dashboardModel: null, globalModel: null, threadsData: null, usersData: null, dashBoardData: null, globalData: null, receivedTheFirstMessage: {} };
+
+global.client = { dirConfig, dirConfigCommands, dirAccount, countDown: {}, cache: {}, database: { creatingThreadData: [], creatingUserData: [], creatingDashBoardData: [], creatingGlobalData: [] }, commandBanned: configCommands.commandBanned };
+
+const utils = require("./utils.js"); global.utils = utils; const { colors } = utils;
+
+global.temp = { createThreadData: [], createUserData: [], createThreadDataError: [], filesOfGoogleDrive: { arraybuffer: {}, stream: {}, fileNames: {} }, contentScripts: { cmds: {}, events: {} } };
+
+const watchAndReloadConfig = (dir, type, prop, logName) => { let lastModified = fs.statSync(dir).mtimeMs; let isFirstModified = true;
+
+fs.watch(dir, (eventType) => {
+    if (eventType === type) {
+        const oldConfig = global.GoatBot[prop];
+
+        setTimeout(() => {
+            try {
+                if (isFirstModified) {
+                    isFirstModified = false;
+                    return;
+                }
+                if (lastModified === fs.statSync(dir).mtimeMs) {
+                    return;
+                }
+                global.GoatBot[prop] = JSON.parse(fs.readFileSync(dir, 'utf-8'));
+                log.success(logName, `Reloaded ${dir.replace(process.cwd(), "")}`);
+            }
+            catch (err) {
+                log.warn(logName, `Can't reload ${dir.replace(process.cwd(), "")}`);
+                global.GoatBot[prop] = oldConfig;
+            }
+            finally {
+                lastModified = fs.statSync(dir).mtimeMs;
+            }
+        }, 200);
+    }
+});
+
 };
 
-global.db = {
-    	threadInfo: new Map(),
-    	threadData: new Map(),
-    	userName: new Map(),
-    	userBanned: new Map(),
-    	threadBanned: new Map(),
-    	commandBanned: new Map(),
-    	threadAllowNSFW: new Array(),
-   	allUserID: new Array(),
-   	allCurrenciesID: new Array(),
-    	allThreadID: new Array()
-	// all data
-	allThreadData: [],
-	allUserData: [],
-	allDashBoardData: [],
-	allGlobalData: [],
 
-	// model
-	threadModel: null,
-	userModel: null,
-	dashboardModel: null,
-	globalModel: null,
-
-	// handle data
-	threadsData: null,
-	usersData: null,
-	dashBoardData: null,
-	globalData: null,
-
-	receivedTheFirstMessage: {}
-
-	// all will be set in bot/login/loadData.js
-};
-
-global.client = {
-	dirConfig,
-	dirConfigCommands,
-	dirAccount,
-	countDown: {},
-	cache: {},
-	database: {
-		creatingThreadData: [],
-		creatingUserData: [],
-		creatingDashBoardData: [],
-		creatingGlobalData: []
-	},
-	commandBanned: configCommands.commandBanned
-};
-
-const utils = require("./utils.js");
-global.utils = utils;
-const { colors } = utils;
-
-global.temp = {
-	createThreadData: [],
-	createUserData: [],
-	createThreadDataError: [], // Can't get info of groups with instagram members
-	filesOfGoogleDrive: {
-		arraybuffer: {},
-		stream: {},
-		fileNames: {}
-	},
-	contentScripts: {
-		cmds: {},
-		events: {}
-	}
-};
-
-// watch dirConfigCommands file and dirConfig
-const watchAndReloadConfig = (dir, type, prop, logName) => {
-	let lastModified = fs.statSync(dir).mtimeMs;
-	let isFirstModified = true;
-
-	fs.watch(dir, (eventType) => {
-		if (eventType === type) {
-			const oldConfig = global.GoatBot[prop];
-
-			// wait 200ms to reload config
-			setTimeout(() => {
-				try {
-					// if file change first time (when start bot, maybe you know it's called when start bot?) => not reload
-					if (isFirstModified) {
-						isFirstModified = false;
-						return;
-					}
-					// if file not change => not reload
-					if (lastModified === fs.statSync(dir).mtimeMs) {
-						return;
-					}
-					global.GoatBot[prop] = JSON.parse(fs.readFileSync(dir, 'utf-8'));
-					log.success(logName, `Reloaded ${dir.replace(process.cwd(), "")}`);
-				}
-				catch (err) {
-					log.warn(logName, `Can't reload ${dir.replace(process.cwd(), "")}`);
-					global.GoatBot[prop] = oldConfig;
-				}
-				finally {
-					lastModified = fs.statSync(dir).mtimeMs;
-				}
-			}, 200);
-		}
-	});
-};
 
 watchAndReloadConfig(dirConfigCommands, 'change', 'configCommands', 'CONFIG COMMANDS');
 watchAndReloadConfig(dirConfig, 'change', 'config', 'CONFIG');
